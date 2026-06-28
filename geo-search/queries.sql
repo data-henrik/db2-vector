d4b1aca910c6c45a391a9b3223c00777eb1b58da
Henrik Loeser <henrik@data-henrik.de>
2026-06-28 13:33:10 +0200

    haversine

--- geo-search/queries.sql
+++ geo-search/queries.sql
@@ -15,4 +15,7 @@ values haversine((select lat from cities where name='Munich'),(select long from 
 
 
 -- some experiment to combine vector distance with actual distance in kilometers
-select name,country, haversine(vec2dec(coord,0), vec2dec(coord,1), vec2dec(coord2,0), vec2dec(coord2,1)) from (select loc2.*, lag (loc2.coord) over (order by num) as coord2 from (select rownumber() over() as num, loc.* from (with loc(name,country,coord,distance) as (values('Helgoland','Germany',vector('[54.179,7.882]',2,float32),0) union all (select name, country, coord, vector_distance(latlngvec(54.179,7.882),coord,euclidean) as distance from cities order by distance limit 15)) select * from loc order by distance) loc) loc2) loc3
+select name,country, haversine(vec2dec(coord,0), vec2dec(coord,1), vec2dec(coord2,0), vec2dec(coord2,1)) from (select loc2.*, lag (loc2.coord) over (order by num) as coord2 from (select rownumber() over() as num, loc.* from (with loc(name,country,coord,distance) as (values('Helgoland','Germany',vector('[54.179,7.882]',2,float32),0) union all (select name, country, coord, vector_distance(latlngvec(54.179,7.882),coord,euclidean) as distance from cities order by distance limit 15)) select * from loc order by distance) loc) loc2) loc3;
+
+
+select name,country,haversine(lat, long, blat, blong) as distance_km from (select a.name, a.country, a.lat, a.long, b.lat blat, b.long blong, vector_distance(b.coord,a.coord,euclidean) as distance from cities a, cities b where b.name='Friedrichshafen' order by distance limit 20);
